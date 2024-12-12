4a8db30a818edda81f29eff28fd27a0e3d7b4bb9
Schwifter <d.goglidze@sangu.edu.ge>
2024-12-12 15:11:39 +0400

    Rename

--- problems/largest_prime_factor/code.py
+++ problems/largest_prime_factor/code.py
@@ -16,9 +16,9 @@ def largest_prime_factor(n: int) -> int:
     return n
 
 
-def is_prime(n: int) -> bool:
-    for divisor in range(2, int(n**0.5) + 1):
-        if n % divisor == 0:
+def is_prime(number: int) -> bool:
+    for divisor in range(2, int(number**0.5) + 1):
+        if number % divisor == 0:
             return False
 
     return True
