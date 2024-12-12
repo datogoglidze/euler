c38f6557b3a7b2cd0ca4f0d221f8e320ae1bf6c1
Schwifter <d.goglidze@sangu.edu.ge>
2024-12-12 15:20:11 +0400

    Use list comprehension

--- problems/largest_prime_factor/code.py
+++ problems/largest_prime_factor/code.py
@@ -3,11 +3,11 @@
 
 
 def largest_prime_factor(n: int) -> int:
-    prime_numbers = []
-
-    for potential_factor in range(2, int(n**0.5) + 1):
-        if is_prime(potential_factor) and n % potential_factor == 0:
-            prime_numbers.append(potential_factor)
+    prime_numbers = [
+        potential_factor
+        for potential_factor in range(2, int(n**0.5) + 1)
+        if is_prime(potential_factor) and n % potential_factor == 0
+    ]
 
     return prime_numbers[-1] if prime_numbers else n
 
