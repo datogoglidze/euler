e1f2955a85f7a506fdf2eb7646ea158ca64f6cd1
Schwifter <d.goglidze@sangu.edu.ge>
2024-12-12 23:51:13 +0400

    Use set

--- problems/largest_palindrome_product/code.py
+++ problems/largest_palindrome_product/code.py
@@ -4,7 +4,7 @@
 
 
 def largest_palindrome_product(n: int) -> int | None:
-    products = [i * k for i in range(10 ** (n - 1), 10**n) for k in range(i, 10**n)]
+    products = {i * k for i in range(10 ** (n - 1), 10**n) for k in range(i, 10**n)}
 
     for i in sorted(products, reverse=True):
         if str(i) == str(i)[::-1]:
