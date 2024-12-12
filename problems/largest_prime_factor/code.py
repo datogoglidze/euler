2ac5adc8921b32cf93049f7546c20d826d6874bb
Schwifter <d.goglidze@sangu.edu.ge>
2024-12-12 15:18:52 +0400

    Refactor

--- problems/largest_prime_factor/code.py
+++ problems/largest_prime_factor/code.py
@@ -6,14 +6,10 @@ def largest_prime_factor(n: int) -> int:
     prime_numbers = []
 
     for potential_factor in range(2, int(n**0.5) + 1):
-        if is_prime(potential_factor):
+        if is_prime(potential_factor) and n % potential_factor == 0:
             prime_numbers.append(potential_factor)
 
-    for number in reversed(prime_numbers):
-        if n % number == 0:
-            return number
-
-    return n
+    return prime_numbers[-1] if prime_numbers else n
 
 
 def is_prime(number: int) -> bool:
