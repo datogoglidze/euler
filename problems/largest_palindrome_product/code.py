6d82d845b7057983b4b6839756c7f625f3587481
Schwifter <d.goglidze@sangu.edu.ge>
2024-12-13 00:20:29 +0400

    Reduce unnecessary iteration

--- problems/largest_palindrome_product/code.py
+++ problems/largest_palindrome_product/code.py
@@ -4,11 +4,15 @@
 
 
 def largest_palindrome_product(n: int) -> int | None:
-    products = []
+    products = [0]
 
     for i in range((10**n) - 1, 10 ** (n - 1) - 1, -1):
         for k in range(i, 10 ** (n - 1) - 1, -1):
             product = i * k
+
+            if product < max(products):
+                break
+
             if str(product) == str(product)[::-1]:
                 products.append(product)
 
